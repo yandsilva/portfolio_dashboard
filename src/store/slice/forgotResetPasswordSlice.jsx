fc9514e73902e6ef566a71e8ec74b768d87026e4
YanDSilva <yandsilva94@gmail.com>
2024-09-19 10:29:02 -0300

    forgot password completed

--- src/store/slice/forgotResetPasswordSlice.jsx
+++ src/store/slice/forgotResetPasswordSlice.jsx
@@ -2,223 +2,96 @@ import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const forgotResetPassSlice = createSlice({
-  name: "user",
+  name: "forgotPassword",
   initialState: {
     loading: false,
-    user: {},
-    isAuthenticated: false,
     error: null,
     message: null,
-    isUpdate: false,
   },
-  // LOGIN =====================
+
   reducers: {
-    loginRequest(state, action) {
+    forgotPasswordRequest(state, action) {
       state.loading = true;
-      state.isAuthenticated = false;
-      state.user = {};
       state.error = null;
+      state.message = null;
     },
 
-    loginSuccess(state, action) {
+    forgotPasswordSuccess(state, action) {
       state.loading = false;
-      state.isAuthenticated = true;
-      state.user = action.payload;
-      state.error = null;
-    },
-
-    loginFailed(state, action) {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.user = {};
-      state.error = action.payload;
-    },
-
-    // LOADUSER ====================
-    loadUserRequest(state, action) {
-      state.loading = true;
-      state.isAuthenticated = false;
-      state.user = {};
-      state.error = null;
-    },
-
-    loadUserSuccess(state, action) {
-      state.loading = false;
-      state.isAuthenticated = true;
-      state.user = action.payload;
-      state.error = null;
-    },
-
-    loadUserFailed(state, action) {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.user = {};
-      state.error = action.payload;
-    },
-
-    // LOGOUT ========================
-
-    logoutSuccess(state, action) {
-      state.loading = false;
-      state.isAuthenticated = false;
-      state.user = {};
       state.error = null;
       state.message = action.payload;
     },
 
-    logoutFailed(state, action) {
+    forgotPasswordFailed(state, action) {
       state.loading = false;
-      state.isAuthenticated = state.isAuthenticated;
-      state.user = state.user;
       state.error = action.payload;
+      state.message = null;
     },
-    // UPDATE PASSWORD ===================
-    updatePasswordRequest(state, action) {
+
+    resetPasswordRequest(state, action) {
       state.loading = true;
-      state.isUpdated = false;
-      state.message = null;
       state.error = null;
+      state.message = null;
     },
 
-    updatePasswordSuccess(state, action) {
+    resetPasswordSuccess(state, action) {
       state.loading = false;
-      state.isUpdated = true;
+      state.error = null;
       state.message = action.payload;
-      state.error = null;
     },
 
-    updatePasswordFailed(state, action) {
+    resetPasswordFailed(state, action) {
       state.loading = false;
-      state.isUpdated = false;
-      state.message = null;
       state.error = action.payload;
-    },
-
-    // UPDATE PROFILE =================
-    updateProfileRequest(state, action) {
-      state.loading = true;
-      state.isUpdated = false;
-      state.message = null;
-      state.error = null;
-    },
-
-    updateProfileSuccess(state, action) {
-      state.loading = false;
-      state.isUpdated = true;
-      state.message = action.payload;
-      state.error = null;
-    },
-
-    updateProfileFailed(state, action) {
-      state.loading = false;
-      state.isUpdated = false;
-      state.message = null;
-      state.error = action.payload;
-    },
-
-    updateProfileResetAfterUpdate(state, action) {
-      state.error = null;
-      state.isUpdated = false;
       state.message = null;
     },
 
     clearAllErrors(state, action) {
       state.error = null;
-      state.user = state.user;
+      state = state;
     },
   },
 });
 
-export const login = (email, password) => async (dispatch) => {
-  dispatch(userSlice.actions.loginRequest());
+export const forgotPassword = (email) => async (dispatch) => {
+  dispatch(forgotResetPassSlice.actions.forgotPasswordRequest());
   try {
     const { data } = await axios.post(
-      "http://localhost:4000/api/v1/user/login",
-      { email, password },
+      "http://localhost:4000/api/v1/user/password/forgot",
+      { email },
       { withCredentials: true, headers: { "Content-Type": "application/json" } }
     );
-    dispatch(userSlice.actions.loginSuccess(data.user));
-    dispatch(userSlice.actions.clearAllErrors());
+    dispatch(forgotResetPassSlice.actions.forgotPasswordSuccess(data.messge));
+    dispatch(forgotResetPassSlice.actions.clearAllErrors());
   } catch (error) {
-    dispatch(userSlice.actions.loginFailed(error.response.data.message));
-  }
-};
-
-export const getUser = () => async (dispatch) => {
-  dispatch(userSlice.actions.loadUserRequest());
-  try {
-    const { data } = await axios.get(
-      "http://localhost:4000/api/v1/user/me",
-
-      { withCredentials: true }
+    forgotResetPassSlice.actions.forgotPasswordFailed(
+      error.response.data.message
     );
-    dispatch(userSlice.actions.loadUserSuccess(data.user));
-    dispatch(userSlice.actions.clearAllErrors());
-  } catch (error) {
-    dispatch(userSlice.actions.loadUserFailed(error.response.data.message));
   }
 };
 
-export const logout = () => async (dispatch) => {
-  try {
-    const { data } = await axios.get(
-      "http://localhost:4000/api/v1/user/logout",
-      { withCredentials: true }
-    );
-    dispatch(userSlice.actions.logoutSuccess(data.message));
-    dispatch(userSlice.actions.clearAllErrors());
-  } catch (error) {
-    dispatch(userSlice.actions.logoutFailed(error.response.data.message));
-  }
-};
-
-export const updatePassword =
-  (currentPassword, newPassword, confirmNewPassword) => async (dispatch) => {
-    dispatch(userSlice.actions.updateProfileResetAfterUpdate());
+export const resetPassword =
+  (token, password, confirmPassword) => async (dispatch) => {
+    dispatch(forgotResetPassSlice.actions.resetPasswordRequest());
     try {
       const { data } = await axios.put(
-        "http://localhost:4000/api/v1/user/update/password",
-        { currentPassword, newPassword, confirmNewPassword },
+        `http://localhost:4000/api/v1/user/password/reset/${token}`,
+        { password, confirmPassword },
         {
           withCredentials: true,
           headers: { "Content-Type": "application/json" },
         }
       );
-      dispatch(userSlice.actions.updatePasswordSuccess(data.message));
-      dispatch(userSlice.actions.clearAllErrors());
+      dispatch(forgotResetPassSlice.actions.resetPasswordSuccess(data.messge));
+      dispatch(forgotResetPassSlice.actions.clearAllErrors());
     } catch (error) {
-      dispatch(
-        userSlice.actions.updatePasswordFailed(error.response.date.message)
+      forgotResetPassSlice.actions.resetPasswordFailed(
+        error.response.data.message
       );
     }
   };
 
-export const updateProfile = (data) => async (dispatch) => {
-  dispatch(userSlice.actions.updateProfileRequest());
-  try {
-    const { data } = await axios.put(
-      "http://localhost:4000/api/v1/user/update/me",
-      { currentPassword, newPassword, confirmNewPassword },
-      data,
-      {
-        withCredentials: true,
-        headers: { "Content-Type": "multipart/form-data" },
-      }
-    );
-    dispatch(userSlice.actions.updateProfileSuccess(data.message));
-    dispatch(userSlice.actions.clearAllErrors());
-  } catch (error) {
-    dispatch(
-      userSlice.actions.updateProfileFailed(error.response.date.message)
-    );
-  }
-};
-
-export const resetProfile = () => (dispatch) => {
-  dispatch(userSlice.actions.clearAllErrors());
-};
-
-export const clearAllUserErrors = () => (dispatch) => {
+export const clearAllForgotPasswordErrors = () => (dispatch) => {
   dispatch(userSlice.actions.clearAllErrors());
 };
 
